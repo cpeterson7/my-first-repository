ec8e86595a914f9f3b64daa19a49012003847141
Clint Peterson <clintepeterson@gmail.com>
2015-03-13 12:42:08 -0700

    Assignment

---
+++ 1stCalculator^.rb
@@ -0,0 +1,20 @@
+puts "Would you like to calculate your Cap rate?"
+answer1 = gets.chomp
+
+if answer1 == 'Yes'
+	puts "What's the value of your property?"
+answer2 = gets.chomp.to_f
+	puts "How much annual income do you receive from your property?"
+answer3 = gets.chomp.to_f
+	puts "What are your annual property expenses?"
+answer4 = gets.chomp.to_f
+	puts "Your NOI is #{answer3.to_f - answer4.to_f}"
+	puts "Your Cap Rate is #{(answer3.to_f - answer4.to_f) / answer2.to_f}"
+elsif answer1 == 'No' 
+	puts "Have a nice day"
+end
+
+
+
+
+
